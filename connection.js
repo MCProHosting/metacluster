628f3fcf8a26c8f12f24bedda0e68984f91bd27b
Connor Peet <connor@connorpeet.com>
2014-09-09 23:12:44 -0400

    Added connection polling!

---
+++ connection.js
@@ -0,0 +1,109 @@
+var log     = require('./log'),
+    config  = require('./config'),
+    parser  = require('./parser'),
+    emitter = require('events').EventEmitter,
+    util    = require('util'),
+    net     = require('net'),
+    async   = require('async'),
+    _       = require('lodash');
+
+function Connection (port, host) {
+    var self       = this,
+        spool      = '',
+        addrString = host + ':' + port,
+        abortAfter = config.performance.failRetries;
+
+    self.attempts = 0;
+
+    self.queue = async.queue(function (query, callback) {
+        self.client.write(query.trim() + parser.delimiter);
+        
+        self.on('chunk', function (data) {
+            callback(data);
+            self.removeAllListeners('chunk');
+        });
+    }, 1);
+
+    // Debounce this to prevent filcker/proxy oddness
+    var isConnected = _.debounce(function () {
+        if (self.status !== 'offline') {
+            self.emit('status', 'online');
+        }
+    }, 5000);
+
+    /**
+     * Attempts to connect to the server. Fails if we're passed the max
+     * number of attempts, and sets a delay exponentially to the past
+     * number of failed attempts.
+     */
+    self.connect = function () {
+        var delay = Math.pow(self.attempts++, 2);
+
+        if (self.error) {
+            log.info('Unable to connect to ' + addrString + ', ' + self.error + ', retrying in ' + delay + 's');
+        }
+
+        if (self.attempts < abortAfter) {
+            return setTimeout(startClient, delay * 1000)
+        }
+
+        log.error('Unable to connect to ' + addrString + ', aborted after '
+            + abortAfter + ' attempts with error' + self.error);
+    };
+
+    /**
+     * Tries to connect to this record's server, binding all
+     * socket events.
+     */
+    function startClient() {
+        self.client = net.createConnection(port, host);
+
+        self.client.on('connect', function () {
+            self.emit('status', 'establishing');
+            isConnected();
+        });
+
+        self.client.on('close', function () {
+            self.emit('status', 'offline');
+            self.error = 'Connection closed.';
+        });
+
+        self.client.on('timeout', function () {
+            self.emit('status', 'offline');
+            self.error = 'Connection timed out.';
+        });
+
+        self.client.on('error', function (e) {
+            self.emit('status', 'offline');
+            self.error = 'Connection error ' + e;
+        });
+
+        self.client.on('data', function (data) {
+            spool += data.toString('utf8');
+            var pos = parser.endIndex(spool);
+
+            if (pos !== -1) {
+                self.emit('chunk', spool.slice(0, pos));
+                spool = spool.slice(pos);
+            }
+        });
+    }
+
+    self.on('status', function (status) {
+        self.status = status;
+
+        if (status === 'offline') {
+            self.connect();
+        } else if (status === 'online') {
+            log.info('Connected to ' + addrString);
+            self.error = null;
+            self.attempts = 0;
+        }
+    });
+
+    self.connect();
+}
+
+util.inherits(Connection, emitter);
+
+module.exports = Connection;
--- record.js
+++ record.js
@@ -1,115 +1,49 @@
-var log     = require('./log'),
-    parser  = require('./parser'),
-    emitter = require('events').EventEmitter,
-    util    = require('util'),
-    net     = require('net'),
-    async   = require('async'),
-    _       = require('lodash');
+var connection = require('./connection'),
+    config     = require('./config'),
+    _          = require('lodash');
 
 function MemcachedRecord (port, host, local) {
-    var self       = this,
-        spool      = '',
-        addrString = host + ':' + port;
+    var self = this,
+        connections = [];
 
-    self.attempts = 0;
-    self.abortAfter = 30;
-    self.local = !! local;
+    // Instantiate our connections
+    while (connections.length < config.performance.connectionPool) {
+        var con = new connection(port, host);
+        con.on('status', pollStatus);
 
-    // Debounce this to prevent filcker/proxy oddness
-    var isConnected = _.debounce(function () {
-        if (self.status !== 'offline') {
-            log.info('Connected to ' + addrString);
-            self.status = 'online';
-            self.error = null;
-            self.attempts = 0;
-        }
-    }, 5000);
-
-    // Queue to process commands in series.
-    var queue = async.queue(function (query, callback) {
-        self.client.write(query.trim() + parser.delimiter);
-
-        self.on('chunk', function (data) {
-            callback(data);
-            self.removeAllListeners('chunk');
-        });
-    }, 1);
+        connections.push(con);
+    }
 
     /**
-     * Writes a command out to the connection, and calls
-     * back with the output.
+     * Writes a command to the least busy of the connections.
      *
      * @param {string} query
      * @param {function} callback
      */
     self.write = function (query, callback) {
-        queue.push(query, callback);
+        var c = _.where(connections, {status: 'online'}).sort(function (a, b) {
+            return a.queue.length() - b.queue.length();
+        });
+
+        if (!c.length) {
+            throw Error('We should not try to connect to nodes which are not online!');
+        }
+
+        c.shift().queue.push(query, callback);
     };
 
     /**
-     * Attempts to connect to the server. Fails if we're passed the max
-     * number of attempts, and sets a delay exponentially to the past
-     * number of failed attempts.
+     * Checks the status of all connections.
      */
-    self.connect = function () {
-        var delay = Math.pow(self.attempts++, 2);
-
-        if (self.error) {
-            log.info('Unable to connect to ' + addrString + ', ' + self.error + ', retrying in ' + delay + 's');
-        }
-
-        if (self.attempts < self.abortAfter) {
-            return setTimeout(startClient, delay * 1000)
-        }
-
-        log.error('Unable to connect to ' + addrString + ', aborted after '
-            + self.abortAfter + ' attempts with error' + self.error);
-    };
-
-    /**
-     * Tries to connect to this record's server, binding all
-     * socket events.
-     */
-    function startClient() {
-        self.client = net.createConnection(port, host);
-
-        self.client.on('connect', function () {
-            self.status = 'establishing';
-            isConnected();
-        });
-
-        self.client.on('close', function () {
-            self.status = 'offline';
-            self.error = 'Connection closed.';
-            self.connect();
-        });
-
-        self.client.on('timeout', function () {
-            self.status = 'offline';
-            self.error = 'Connection timed out.';
-            self.connect();
-        });
-
-        self.client.on('error', function (e) {
-            self.status = 'offline';
-            self.error = 'Connection error ' + e;
-            self.connect();
-        });
-
-        self.client.on('data', function (data) {
-            spool += data.toString('utf8');
-            var pos = parser.endIndex(spool);
-
-            if (pos !== -1) {
-                self.emit('chunk', spool.slice(0, pos));
-                spool = spool.slice(pos);
+    function pollStatus () {
+        self.status = 'offline';
+        for (var i = 0, l = connections.length; i < l; i++) {
+            if (connections[i].status === 'online') {
+                self.status = 'online';
+                break;
             }
-        });
+        }
     }
-
-    self.connect();
 }
-
-util.inherits(MemcachedRecord, emitter);
 
 module.exports = MemcachedRecord;
